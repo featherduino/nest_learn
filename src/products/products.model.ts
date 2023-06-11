532c6d17f80ab7a8fa875c68f45fd7f1ca597adb
featherduino <shivamchowdhary93@gmail.com>
2023-06-12 03:47:54 +0530

    monorepo for react in nest...

--- src/products/products.model.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-export class Product{
-    constructor(
-        public id:string,
-        public title:string,
-        public description:string,
-        public price:number,
-        ) {}
-    }
